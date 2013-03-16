fd86d9ec2166035fff0228a1ffd9aaaa76450156
raven <raven@raven-Dell-System-Inspiron-N7110.(none)>
2013-03-16 11:30:51 +0530

    Sofa initial push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sofatest::Application.config.secret_token = '16cf4224fc1a47783664fbcaac350ede13a6046fc7e609f07ad190814a2be8da065f8d3849ac5b53b060b5e3f285470ffcf7fed14206b6a3b80ac8ff06a2a31f'
